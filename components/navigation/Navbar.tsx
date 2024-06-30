d62af20aa0a1bf5eb4ba30c92063f8166a7c965e
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-06-30 23:54:39 +0900

    feat: Clerk 인증 추가
    * feat: Navbar에 Clerk 컴포넌트추가
    
    * feat: 특정 경로 인증을 위한 Clerk middleware 설정
    
    * feat: app/layout.tsx ClerkProvider 추가
    
    * feat: Clerk를 사용하여 SignIn 페이지 추가
    
    * feat: Clerk를 사용하여 SignUp 페이지 추가

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
-import { FaHome } from "react-icons/fa";
+
+import { FaHome, FaUserFriends } from "react-icons/fa";
 import { GiThreeFriends } from "react-icons/gi";
 import { CiCirclePlus } from "react-icons/ci";
+import { RiLoginBoxFill } from "react-icons/ri";
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -12,23 +25,57 @@ const Navbar = () => {
           Social
         </Link>
       </div>
-      <div className="hidden md:flex w-[50%]">
+      <div className="hidden md:flex w-[50%] items-center justify-between">
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex gap-2 items-center">
-            <FaHome />
+            <FaHome size={30} />
             <span>HomePage</span>
           </Link>
           <Link href="/" className="flex gap-2 items-center">
-            <GiThreeFriends />
+            <GiThreeFriends size={30} />
             <span>Friends</span>
           </Link>
           <Link href="/" className="flex gap-2 items-center">
-            <CiCirclePlus />
+            <CiCirclePlus size={30} />
             <span>Stories</span>
           </Link>
         </div>
+        <div className="hidden xl:flex items-center bg-slate-100 rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <FaMagnifyingGlass />
+        </div>
       </div>
       <div className="w-[30%] flex justify-end">
+        <ClerkLoading>
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="hidden md:flex gap-4 cursor-pointer mr-4">
+              <FaUserFriends size={30} />
+              <AiOutlineMessage size={30} />
+              <IoIosNotificationsOutline size={30} />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2">
+              <RiLoginBoxFill size={30} />
+              <Link href="/sign-in">Login / Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
