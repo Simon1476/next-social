395b6903e6266b9e5acf666262d323bdaf340978
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-03 22:36:47 +0900

    feat: 홈 페이지 레이아웃 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Feed from "@/components/Feed";
+import LeftSideBar from "@/components/LeftSideBar";
+import PostComposer from "@/components/PostComposer";
+import RightSideBar from "@/components/RightSideBar";
+import UserStorties from "@/components/UserStorties";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="flex gap-6 pt-6">
+      <div className="hidden xl:block w-[20%]">
+        <LeftSideBar type="home" />
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <UserStorties />
+          <PostComposer />
+          <Feed />
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightSideBar />
+      </div>
     </main>
   );
 }
