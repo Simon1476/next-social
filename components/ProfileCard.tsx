444f4939ec85cae63071362255076677f05cdc3b
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-05 23:56:59 +0900

    feat: 정적 Profile 데이터를 Prisma의 동적 사용자 데이터로 교체

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,17 +1,38 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const ProfileCard = () => {
+const ProfileCard = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+        },
+      },
+    },
+  });
+
+  if (!user) return null;
+
   return (
     <div className="flex flex-col gap-6 p-4 bg-white rounded-lg shadow-md text-sm">
       <div className="h-20 relative">
         <Image
-          src="https://images.pexels.com/photos/315191/pexels-photo-315191.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt="Profile Image"
+          src={user.cover || "/noCover.jpg"}
+          alt="Cover Image"
           fill
           className="rounded-md object-cover"
         />
         <Image
-          src="https://images.pexels.com/photos/402028/pexels-photo-402028.jpeg?auto=compress&cs=tinysrgb&w=600"
+          src={user.avatar || "/noAvatar.png"}
           alt="Profile Image"
           width={48}
           height={48}
@@ -19,7 +40,11 @@ const ProfileCard = () => {
         />
       </div>
       <div className="flex flex-col gap-2 items-center h-20">
-        <span className="font-semibold">Oliver Johnson</span>
+        <span className="font-semibold">
+          {user.name && user.surname
+            ? user.name + " " + user.surname
+            : user.username}
+        </span>
         <div className="flex items-center gap-4">
           <div className="flex">
             <Image
@@ -44,7 +69,9 @@ const ProfileCard = () => {
               className="w-3 h-3 object-cover rounded-full"
             />
           </div>
-          <span className="text-xs text-gray-500">777 Followers</span>
+          <span className="text-xs text-gray-500">
+            {user._count.followers} Followers
+          </span>
         </div>
         <button className="rounded-md p-2 text-white bg-blue-500">
           My Profile
