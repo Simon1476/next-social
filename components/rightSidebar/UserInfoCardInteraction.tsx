fad86565bda520d0e5ec0a2767d5ce832513992c
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-10 22:46:36 +0900

    refactor: UserInfoCardInteraction 컴포넌트에 낙관적 UI 및 상태 처리 기능 추가

--- components/rightSidebar/UserInfoCardInteraction.tsx
+++ components/rightSidebar/UserInfoCardInteraction.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+"use client";
+
+import { toggleFollowStatus, toggleBlockStatus } from "@/lib/actions";
+import { useOptimistic, useState } from "react";
+
+type UserState = {
+  following: boolean;
+  blocked: boolean;
+  followingRequestSent: boolean;
+};
 
 type Props = {
   userId: string;
-  currentUserId: string;
+
   isUserBlocked: boolean;
   isFollowing: boolean;
   isFollowingSent: boolean;
@@ -10,26 +19,86 @@ type Props = {
 
 const UserInfoCardInteraction = ({
   userId,
-  currentUserId,
   isUserBlocked,
   isFollowing,
   isFollowingSent,
 }: Props) => {
+  const [userState, setUserState] = useState<UserState>({
+    following: isFollowing,
+    blocked: isUserBlocked,
+    followingRequestSent: isFollowingSent,
+  });
+
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const follow = async () => {
+    switchOptimisticState("follow");
+    setIsProcessing(true);
+    try {
+      await toggleFollowStatus(userId);
+      setUserState((prev) => ({
+        ...prev,
+        following: prev.following && false,
+        followingRequestSent:
+          !prev.following && !prev.followingRequestSent ? true : false,
+      }));
+    } catch (error) {
+      console.error("Failed to toggle follow status:", error);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
+  const block = async () => {
+    switchOptimisticState("block");
+    setIsProcessing(true);
+    try {
+      await toggleBlockStatus(userId);
+      setUserState((prev) => ({
+        ...prev,
+        blocked: !prev.blocked,
+      }));
+    } catch (error) {
+      // Handle the error, possibly reverting the optimistic update
+      console.error("Failed to toggle block status:", error);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
+  const [optimisticState, switchOptimisticState] = useOptimistic(
+    userState,
+    (state, value: "follow" | "block") =>
+      value === "follow"
+        ? {
+            ...state,
+            following: state.following && false,
+            followingRequestSent:
+              !state.following && !state.followingRequestSent ? true : false,
+          }
+        : { ...state, blocked: !state.blocked }
+  );
+
   return (
     <>
-      <form action="">
-        <button className="w-full p-2 bg-blue-500 text-white text-sm rounded-md">
-          {isFollowing
+      <form action={follow}>
+        <button
+          disabled={isProcessing}
+          className="w-full p-2 bg-blue-500 text-white text-sm rounded-md"
+        >
+          {optimisticState.following
             ? "Following"
-            : isFollowingSent
+            : optimisticState.followingRequestSent
             ? "Friend Request Sent"
             : "Follow"}
         </button>
       </form>
-      <form action="" className="self-end">
-        <span className="text-red-400 text-xs cursor-pointer">
-          {isUserBlocked ? "Unblock User" : "Block User"}
-        </span>
+      <form action={block} className="self-end">
+        <button disabled={isProcessing}>
+          <span className="text-red-400 text-xs cursor-pointer">
+            {optimisticState.blocked ? "Unblock User" : "Block User"}
+          </span>
+        </button>
       </form>
     </>
   );
