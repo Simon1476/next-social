65e20594b4baf449733f9a0557df050d4aadc167
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-12 21:42:26 +0900

    feat: Post 컴포넌트에 동적 데이터 사용 및 PostInteraction 컴포넌트 추가

--- components/feed/Post.tsx
+++ components/feed/Post.tsx
@@ -1,77 +1,54 @@
 import Image from "next/image";
 import { HiDotsHorizontal } from "react-icons/hi";
-import { FaRegThumbsUp, FaCommentDots, FaShare } from "react-icons/fa";
 import Comments from "./Comments";
-const Post = () => {
+import { Post as PostType, User } from "@prisma/client";
+import PostInteraction from "./PostInteraction";
+
+type FeedPostsType = PostType & { user: User } & {
+  likes: [{ userId: string }];
+} & { _count: { comments: number } };
+
+const Post = ({ post }: { post: FeedPostsType }) => {
   return (
     <div className="flex flex-col gap-4">
       {/* USER */}
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-4">
           <Image
-            src="https://images.pexels.com/photos/24244035/pexels-photo-24244035.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            src={post.user.avatar || "/noAvatar.png"}
             alt=""
             width={40}
             height={40}
             className="w-10 h-10 rounded-full"
           />
-          <span className="font-md">Alex Johnson</span>
+          <span className="font-md">
+            {post.user.name && post.user.surname
+              ? post.user.name + " " + post.user.surname
+              : post.user.name}
+          </span>
         </div>
         <HiDotsHorizontal />
       </div>
       {/* DESC */}
       <div className="flex flex-col gap-4">
-        <div className="w-full min-h-96 relative">
-          <Image
-            src="https://images.pexels.com/photos/12984738/pexels-photo-12984738.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, aliquam
-          accusamus. Reiciendis molestiae aperiam sapiente, optio, itaque
-          provident eos facilis deserunt vel doloribus minus quo illo iste ut
-          placeat recusandae.
-        </p>
+        {post.img && (
+          <div className="w-full min-h-96 relative">
+            <Image
+              src={post.img}
+              alt="post image"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        )}
+        <p>{post.desc}</p>
       </div>
       {/* INTERACTION */}
-      <div className="flex justify-between items-center text-sm my-4">
-        <div className="flex gap-8">
-          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-            <FaRegThumbsUp
-              size={16}
-              color="#3D72CC"
-              className="cursor-pointer"
-            />
-            <span className="text-gray-300">|</span>
-            <span className="text-gray-500">
-              123 <span className="hidden md:inline"> Likes</span>
-            </span>
-          </div>
-          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-            <FaCommentDots
-              size={16}
-              color="#3D72CC"
-              className="cursor-pointer"
-            />
-            <span className="text-gray-300">|</span>
-            <span className="text-gray-500">
-              123 <span className="hidden md:inline"> Comments</span>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-            <FaShare size={16} className="cursor-pointer" color="#3D72CC" />
-            <span className="text-gray-300">|</span>
-            <span className="text-gray-500">
-              123 <span className="hidden md:inline"> Shares</span>
-            </span>
-          </div>
-        </div>
-      </div>
+      <PostInteraction
+        postId={post.id}
+        likes={post.likes.map((like) => like.userId)}
+        commentNumber={post._count.comments}
+      />
       <Comments />
     </div>
   );
