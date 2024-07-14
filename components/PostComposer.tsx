c0f44d8710a5bad4d2893b4097f3d0928d99bf1e
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-14 17:27:20 +0900

    feat: 게시글 작성 기능 추가
    * feat: form 제출 상태를 나타내는 AddPostButton 컴포넌트 추가
    
    * feat: 이미지 업로드 기능 추가
    
    * feat: addPost 액션 추가

--- components/PostComposer.tsx
+++ components/PostComposer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { BsEmojiKissFill } from "react-icons/bs";
 import { IoMdPhotos } from "react-icons/io";
@@ -5,37 +7,75 @@ import { FaVideo } from "react-icons/fa6";
 import { FaPoll } from "react-icons/fa";
 import { IoCalendarNumberOutline } from "react-icons/io5";
 
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import AddPostButton from "./AddPostButton";
+import { addPost } from "@/lib/actions";
+
 const PostComposer = () => {
+  const { user, isLoaded } = useUser();
+  const [desc, setDesc] = useState("");
+  const [img, setImg] = useState<any>();
+
+  if (!isLoaded) {
+    return "Loading...";
+  }
+
   return (
     <div className="flex justify-between gap-4 p-4 shadow-md bg-white rounded-lg text-sm">
       {/* AVATAR */}
-      <Image
-        src="https://images.pexels.com/photos/7654136/pexels-photo-7654136.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-        alt=""
-        width={48}
-        height={48}
-        className="w-12 h-12 object-cover rounded-full"
-      />
+      {user && (
+        <Image
+          src={user?.imageUrl || "/noAvatar.png"}
+          alt=""
+          width={48}
+          height={48}
+          className="w-12 h-12 object-cover rounded-full"
+        />
+      )}
 
       {/* POST */}
       <div className="flex-1">
         {/* TEXT INPUT */}
-
-        <div className="flex gap-4">
+        <form
+          action={(formData) => addPost(formData, img?.secure_url || "")}
+          className="flex gap-4"
+        >
           <textarea
             placeholder="what are you thinking?"
             name="desc"
             className="flex-1 bg-slate-100 rounded-lg p-2"
+            onChange={(e) => setDesc(e.target.value)}
           ></textarea>
-          <BsEmojiKissFill color="orange" size={20} className="self-end" />
-        </div>
+          <div>
+            <BsEmojiKissFill color="orange" size={20} className="self-end" />
+            <AddPostButton />
+          </div>
+        </form>
         {/* POST OPTIONS */}
 
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
-          <div className="flex items-center gap-2 cursor-pointer">
-            <IoMdPhotos size={20} />
-            Photo
-          </div>
+          <CldUploadWidget
+            uploadPreset="social"
+            onSuccess={(result, { widget }) => {
+              setImg(result.info);
+              widget.close();
+            }}
+          >
+            {({ open }) => {
+              return (
+                <div
+                  className="flex items-center gap-2 cursor-pointer"
+                  onClick={() => open()}
+                >
+                  <IoMdPhotos size={20} />
+                  Photo
+                </div>
+              );
+            }}
+          </CldUploadWidget>
+
           <div className="flex items-center gap-2 cursor-pointer">
             <FaVideo size={20} />
             Video
