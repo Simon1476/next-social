0060fa4ac1992ce9e56aa97f45650ce190da6c9d
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-15 11:21:58 +0900

    feat: ProfileCard에 Link 추가

--- components/leftSidebar/ProfileCard.tsx
+++ components/leftSidebar/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/client";
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import Link from "next/link";
 
 const ProfileCard = async () => {
   const { userId } = auth();
@@ -73,9 +74,11 @@ const ProfileCard = async () => {
             {user._count.followers} Followers
           </span>
         </div>
-        <button className="rounded-md p-2 text-white bg-blue-500">
-          My Profile
-        </button>
+        <Link href={`/profile/${user.username}`}>
+          <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+            My Profile
+          </button>
+        </Link>
       </div>
     </div>
   );
