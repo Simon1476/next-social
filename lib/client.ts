fc94b6e2d5615eb5403124974521a53d3a9e693f
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-17 23:34:30 +0900

    feat: production 환경 prisma 생성 코드 추가

--- lib/client.ts
+++ lib/client.ts
@@ -2,7 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+let prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.CURRENT_ENV == "production") {
+  prisma = new PrismaClient();
+} else {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
