86aa0f6be5c2d398039465f99db16324bc3c3dde
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-07-18 17:43:12 +0900

    fix: Webpack 재설정

--- next.config.mjs
+++ next.config.mjs
@@ -26,13 +26,10 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
-    config.resolve.alias["@"] = path.join(__dirname, "src");
-    config.resolve.alias["@app"] = path.join(__dirname, "src/app");
-    config.resolve.alias["@components"] = path.join(
-      __dirname,
-      "src/components"
-    );
-    config.resolve.alias["@lib"] = path.join(__dirname, "src/lib");
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./src/"),
+    };
     return config;
   },
 };
